40199ec1999930071112baafa0e441e501fee025
Barkah Aji <barkah993@gmail.com>
2025-03-06 11:38:19 +0700

    add new version of the game

---
+++ scriptv2.js
@@ -0,0 +1,16 @@
+// person identitiy objects: name, age, zodiacs,
+const person = {
+    name: 'barkah',
+    age: 18,
+    zodiacs: 'pisces'
+};
+
+//return love array message based on name length
+
+
+
+// return friendship message based on age
+
+// return finance message based on zodiacs
+
+// display all the message
